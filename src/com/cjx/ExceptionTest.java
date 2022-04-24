62bcecd3d1543e0cb6b7ca684471b44cc5dbe04c
chenjunxian <chenjunxian@mobo360.com>
2022-04-24 17:30:32 +0800

    异常测试

---
+++ src/com/cjx/ExceptionTest.java
@@ -0,0 +1,30 @@
+package com.cjx;
+
+public class ExceptionTest {
+    public static void main(String[] args) {
+        int[] b = new int[10];
+
+
+        int a = -100;
+        try {
+            exceptionTest1(a);
+        }catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+//    public static void exceptionTest1() throws NumberFormatException {
+//        String s = "abv";
+//        Integer integer = Integer.parseInt(s);
+//    }
+    public static void exceptionTest1(int a) throws NumberFormatException{
+    if(a>=0){
+        System.out.println("aa");
+    }else{
+        throw new NumberFormatException("数字异常");
+    }
+
+    }
+}
