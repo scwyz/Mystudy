6bfb3ac032bbb338b1e223d66cd404a70c394d02
chenjunxian <chenjunxian@mobo360.com>
2022-04-26 17:29:16 +0800

    20220426

--- src/com/cjx/ThreadTest.java
+++ src/com/cjx/ThreadTest.java
@@ -2,30 +2,65 @@ package com.cjx;
 
 public class ThreadTest {
     public static void main(String[] args) {
-        MyThread myThread = new MyThread("mythread2");
-//        myThread.setName("mythread");
-        myThread.start();
+//        MyThread myThread = new MyThread("mythread2");
+////        myThread.setName("mythread");
+//        myThread.start();
+//        Thread.currentThread().setName("主线程");
+//        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+//        for (int i = 0; i < 100; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(Thread.currentThread().getName() + ":" + i);
+//            }
+//        }
 
 //        for (int i = 0; i < 100; i++) {
 //            if( i % 2 != 0){
 //                System.out.println(i+Thread.currentThread().getName());
 //            }
 //        }
+    MyThread2 myThread2 = new MyThread2();
+    Thread thread = new Thread(myThread2);
+    Thread thread2 = new Thread(myThread2);
+    Thread thread3 = new Thread(myThread2);
+    thread.start();
+    thread2.start();
+    thread3.start();
+
     }
 }
-
-class MyThread extends Thread{
+//创建多线程的第一种方法
+class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            if( i % 2 == 0){
-                System.out.println(i+Thread.currentThread().getName());
+            if (i % 2 == 0) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+            if (i % 20 == 0) {
+                yield();
             }
         }
     }
-    public MyThread(){
+
+    public MyThread() {
     }
-    public MyThread(String a){
+
+    public MyThread(String a) {
         super(a);
+    }
+
+}
+//创建多线程第二种
+class MyThread2 implements Runnable{
+    int ticket = 100;
+    @Override
+    public void run() {
+        while( true ){
+            if(ticket>0){
+                System.out.print(ticket--+" ");
+            }else {
+                break;
+            }
+        }
     }
 }
---
+++ src/com/cjx/WindowTest.java
@@ -0,0 +1,27 @@
+package com.cjx;
+
+public class WindowTest {
+    public static void main(String[] args) {
+    Window ww =  new Window();
+    Window ww2 =  new Window();
+    Window ww3 =  new Window();
+    ww.start();
+    ww2.start();
+    ww3.start();
+    }
+}
+
+class Window extends Thread{
+    static int ticket = 100;
+    @Override
+    public void run(){
+        while( true ){
+            if(ticket>0){
+            System.out.print(ticket--+" ");
+            }else {
+                break;
+            }
+        }
+    }
+
+}
