9524fcf0ec09c1d82dabe319aff26f534beb3cf8
chenjunxian <chenjunxian@mobo360.com>
2022-04-27 17:29:25 +0800

    20220427

--- src/com/cjx/ThreadTest.java
+++ src/com/cjx/ThreadTest.java
@@ -2,13 +2,14 @@ package com.cjx;
 
 public class ThreadTest {
     public static void main(String[] args) {
+        //一
 //        MyThread myThread = new MyThread("mythread2");
 ////        myThread.setName("mythread");
 //        myThread.start();
 //        Thread.currentThread().setName("主线程");
 //        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
-//        for (int i = 0; i < 100; i++) {
-//            if (i % 2 == 0) {
+//        for (int i = 0; i < 50; i++) {
+//            if (i % 1 == 0) {
 //                System.out.println(Thread.currentThread().getName() + ":" + i);
 //            }
 //        }
@@ -18,26 +19,34 @@ public class ThreadTest {
 //                System.out.println(i+Thread.currentThread().getName());
 //            }
 //        }
-    MyThread2 myThread2 = new MyThread2();
-    Thread thread = new Thread(myThread2);
-    Thread thread2 = new Thread(myThread2);
-    Thread thread3 = new Thread(myThread2);
-    thread.start();
-    thread2.start();
-    thread3.start();
+
+        //二
+
+        MyThread2 myThread2 = new MyThread2();
+        Thread thread = new Thread(myThread2);
+        Thread thread2 = new Thread(myThread2);
+        Thread thread3 = new Thread(myThread2);
+        thread.start();
+        thread.setName("线程一");
+        thread2.start();
+        thread2.setName("线程二");
+        thread3.start();
+        thread3.setName("线程二");
 
     }
 }
+
 //创建多线程的第一种方法
 class MyThread extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
+            if (i % 10 == 0) {
+                System.out.println("yield:" + i);
+                yield();
+            }
             if (i % 2 == 0) {
                 System.out.println(Thread.currentThread().getName() + ":" + i);
-            }
-            if (i % 20 == 0) {
-                yield();
             }
         }
     }
@@ -50,17 +59,29 @@ class MyThread extends Thread {
     }
 
 }
+
 //创建多线程第二种
-class MyThread2 implements Runnable{
-    int ticket = 100;
+class MyThread2 implements Runnable {
+    int ticket = 50;
+
     @Override
     public void run() {
-        while( true ){
-            if(ticket>0){
-                System.out.print(ticket--+" ");
-            }else {
-                break;
+
+        while (true) {
+            synchronized (new Object()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (ticket > 0) {
+                    System.out.println(ticket-- + " " + Thread.currentThread().getName());
+                } else {
+                    break;
+                }
+
             }
+
         }
     }
 }
